8fed72f6ed81f3b7687ef27000ba88769115a44c
Sammy Ferrier <sammy.ferrier@braineet.com>
2022-06-13 14:30:32 +0200

    ajout placeholder components

---
+++ frontend/src/pages/auth/login/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Login = () => {
+    return(<></>)
+}
+
+export default Login;
---
+++ frontend/src/pages/evenement/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Evenement = () => {
+    return(<></>)
+}
+
+export default Evenement;
