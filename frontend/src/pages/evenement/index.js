fd31551e51c395ac363b54791d0f252cec7f6abd
Sammy Ferrier <sammy.ferrier@braineet.com>
2022-06-26 19:17:25 +0200

    added routing package and some sample of how it works

--- frontend/src/pages/evenement/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Evenement = () => {
-    return(<></>)
-}
-
-export default Evenement;
---
+++ frontend/src/pages/joingroup/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const JoinGroup = () => {
+    return (
+      <main style={{ padding: "1rem 0" }}>
+        <h2>JoinGroup</h2>
+      </main>
+    );
+  }
+
+export default JoinGroup;
