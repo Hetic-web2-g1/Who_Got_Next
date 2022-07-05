75cf48548877016aeade696ee75b3ffa2da95fee
domino659 <martin@sionfamily.com>
2022-07-05 19:05:37 +0200

    added crud for user, delete is working, update and create incomplete

--- backend/src/routers/users.py
+++ backend/src/routers/users.py
@@ -12,17 +12,53 @@ router = APIRouter(
 )
 
 
+# Get all user
 @router.get("", response_model=List[User | None])
 def get_all_users():
     with engine.begin() as conn:
         return list(UserManager.get_all_users(conn))
 
 
+# Get one user by id
 @router.get("/{user_id}", response_model=User)
 def get_user(user_id: str):
     with engine.begin() as conn:
-        user =  UserManager.get_user_by_id(conn, user_id)
+        user = UserManager.get_user_by_id(conn, user_id)
         if user is None:
             raise HTTPException(404, "User not found")
         else:
             return user
+
+
+# Create User
+@router.post("/create")
+def create_user(user):
+    with engine.begin() as conn:
+        user = UserManager.create_user(conn, user)
+        if user == 0:
+            return False
+        else:
+            return True
+
+
+# Update user by id
+@router.put("/update/{user_id}")
+def update_user(user):
+    with engine.begin() as conn:
+        user = UserManager.update_user(conn, user)
+        if user == 0:
+            return False
+        else:
+            return True
+# TODO Tester et vérifier les inputs a envoyer a user missing data to test
+
+
+# Delete one user by id
+@router.delete("/delete/{user_id}", response_model=bool)
+def delete_user(user_id: str):
+    with engine.begin() as conn:
+        user = UserManager.delete_user_by_id(conn, user_id)
+        if user == 0:
+            return False
+        else:
+            return True
