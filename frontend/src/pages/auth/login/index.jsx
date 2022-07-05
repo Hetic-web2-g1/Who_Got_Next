3b7f5f3e567ec288ae66f2780f5393f16056d671
unknown <alexandre912323@gmail.com>
2022-07-05 10:33:44 +0200

    Left page of login

--- frontend/src/pages/auth/login/index.jsx
+++ frontend/src/pages/auth/login/index.jsx
@@ -1,15 +1,49 @@
 import React from 'react'
+import './index.css'
 import {Link} from 'react-router-dom'
 
-export const JoinGroup = () => {
+export const Login = () => {
     return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Login</h2>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
+      <main>
+        <div className='container flex'>
+
+          <div className='connect-container'>
+
+            <div className='flex'>
+                <h1>Se connecter</h1>
+            </div>
+
+            <div className='flex'>
+                <h4>Ask Who Got Next</h4>
+            </div>
+
+            <div className='hr-div flex'>
+          
+              <div className='line'></div>
+
+              <span>Ou</span>
+              <div className='line'></div>
+
+            </div>
+
+            <div className='form'>
+              <div className='flex-field'>
+                <label htmlFor="mail">Mail</label>
+                <input placeholder='Entrez votre mail' type="text" />
+              </div>
+
+              <div className='flex-field margin'>
+                <label htmlFor="password">Mot de passe</label>
+                <input className='input' placeholder='Entre votre mot de passe' type="password" />
+                <img src="./../../../../public/assets/eye.svg" alt="tamere" />
+              </div>
+            </div>
+
+          </div>
+
+        </div>
       </main>
     );
   }
 
-export default JoinGroup;
+export default Login;
