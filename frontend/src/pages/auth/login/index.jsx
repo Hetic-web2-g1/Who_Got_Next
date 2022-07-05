f3b74724630c229a5a3917eb3f0e3c3392a5d9d0
unknown <alexandre912323@gmail.com>
2022-07-05 18:17:49 +0200

    some advancements

--- frontend/src/pages/auth/login/index.jsx
+++ frontend/src/pages/auth/login/index.jsx
@@ -5,43 +5,58 @@ import {Link} from 'react-router-dom'
 export const Login = () => {
     return (
       <main>
-        <div className='container flex'>
 
-          <div className='connect-container'>
+        <div className='grid-container'>
+          <div className='container flex'>
 
-            <div className='flex'>
-                <h1>Se connecter</h1>
-            </div>
-
-            <div className='flex'>
-                <h4>Ask Who Got Next</h4>
-            </div>
-
-            <div className='hr-div flex'>
-          
-              <div className='line'></div>
-
-              <span>Ou</span>
-              <div className='line'></div>
-
-            </div>
-
-            <div className='form'>
-              <div className='flex-field'>
-                <label htmlFor="mail">Mail</label>
-                <input placeholder='Entrez votre mail' type="text" />
+            <div className='connect-container'>
+              <div className='img-container'>
+                <img src="./../../../../public/assets/logo.svg" alt="logo" />
               </div>
 
-              <div className='flex-field margin'>
-                <label htmlFor="password">Mot de passe</label>
-                <input className='input' placeholder='Entre votre mot de passe' type="password" />
-                <img src="./../../../../public/assets/eye.svg" alt="tamere" />
+              <div className='flex'>
+                  <h1 className='connect-title'>Se connecter</h1>
               </div>
-            </div>
 
+              <div className='flex'>
+                  <h4>Ask Who Got Next</h4>
+              </div>
+
+              <div className='hr-div flex'>
+            
+                <div className='line'></div>
+
+                <span>Ou</span>
+                <div className='line'></div>
+
+              </div>
+
+              <form className='form'>
+                <div className='flex-field'>
+                  <label htmlFor="mail">Mail</label>
+                  <input placeholder='Entrez votre mail' type="text" />
+                </div>
+
+                <div className='flex-field margin'>
+                  <label htmlFor="password">Mot de passe</label>
+                  <input className='input' placeholder='Entrez votre mot de passe' type="password" />
+                  <img src="./../../../../public/assets/eye.svg" alt="see password" />
+                </div>
+
+              </form>
+
+            </div>
           </div>
 
+          <div className='r-image-container'>
+            <img src="./../../../../public/assets/right-login.png" alt="" />
+          </div>
         </div>
+
+        <div className='basket-img'>
+          <img draggable='false' src="./../../../../public/assets/basket.svg" alt="" />
+        </div>
+
       </main>
     );
   }
