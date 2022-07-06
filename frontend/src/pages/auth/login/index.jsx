1ede3a196a0e4289cbe9d8ebc84fc49de5f195fe
unknown <alexandre912323@gmail.com>
2022-07-06 14:44:35 +0200

    Begin continue with google option

--- frontend/src/pages/auth/login/index.jsx
+++ frontend/src/pages/auth/login/index.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react'
+import jwt_decode from 'jwt-decode'
 import './index.css'
 import LandingRedirectionButton
  from '../../../components/landingRedirectionButton'
 import {Link} from 'react-router-dom'
 
 export const Login = () => {
+
+  const [ user, setUser ] = useState({});
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+    var usrObject = jwt_decode(response.credential);
+    console.log(usrObject);
+    setUser(usrObject);
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "709733567706-5vqp12ru9cjqevbeetpvudn8qoube6hb.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large"}
+    );
+
+    google.accounts.id.prompt();
+  }, []);
+
+  // No user => Sign in button
+  // User => Log out button
+
     return (
       <main>
 
@@ -23,6 +51,14 @@ export const Login = () => {
               <div className='flex title'>
                   <h4>Ask Who Got Next</h4>
               </div>
+
+              <div className='flex' id='signInDiv'></div>
+              { user &&
+                <div>
+                  <img src={user.picture}></img>
+                  <h3>{user.name}</h3>
+                </div>
+              }
 
               <div className='hr-div flex'>
             
