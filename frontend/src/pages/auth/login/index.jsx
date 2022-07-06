807b215321a9ec6758ba9867106c6b7d3707058e
Spudwebb96 <jonas.janko17@gmail.com>
2022-07-06 11:07:58 +0200

    prout

--- frontend/src/pages/auth/login/index.jsx
+++ frontend/src/pages/auth/login/index.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import './index.css'
 import {Link} from 'react-router-dom'
@@ -65,20 +64,3 @@ export const Login = () => {
   }
 
 export default Login;
-=======
-import React from "react";
-import { Link } from "react-router-dom";
-
-export const Login = () => {
-  return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Login</h2>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-    </main>
-  );
-};
-
-export default Login;
->>>>>>> main
