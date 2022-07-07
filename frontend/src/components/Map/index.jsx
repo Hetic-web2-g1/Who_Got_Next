d6135ccd5d91efce724af42ed478adfae1b1d748
Erzenkell <ferrier.sammy@gmail.com>
2022-07-07 14:44:39 +0200

    wip

--- frontend/src/components/Map/index.jsx
+++ frontend/src/components/Map/index.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useRef } from "react";
+import React, { useMemo, useState, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import mapboxgl from "mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 import Map, {
@@ -8,57 +8,64 @@ import Map, {
   FullscreenControl,
   ScaleControl,
   GeolocateControl,
-  Layer,
 } from "react-map-gl";
 import Pin from "./Marker";
 import "./styles.css";
-
-import { useFetchCircle } from '../../services/mapFetch'
 
 const TOKEN = (mapboxgl.accessToken =
   "pk.eyJ1IjoiZXJ6ZW5rZWwiLCJhIjoiY2t6eHZiempyMDRoZzJucDlmcmxjeTZjcyJ9.aJWheE8snFrd21W1ElV4_g");
 
 const MapRender = ({ userLongitude, userLatitude }) => {
-  const mapRef = useRef("nique")
   const [lng, setLng] = useState(userLongitude ? userLongitude : 0);
   const [lat, setLat] = useState(userLatitude ? userLatitude : 0);
   const [radius, setRadius] = useState(0);
-  const [zoom, setZoom] = useState(15)
-  const [fields, setFields] = useState()
+  const [zoom, setZoom] = useState(15);
+  const [fields, setFields] = useState();
   const [popupInfo, setPopupInfo] = useState(null);
   const southWest = new mapboxgl.LngLat(-5.0, 42.5);
   const northEast = new mapboxgl.LngLat(9.56, 51.15);
   const bounds = [southWest, northEast];
 
-  const pins = useMemo(
-    () =>
-      fields?.data.map((field) => (
-        <Marker
-          key={`marker-${field.id}`}
-          longitude={field.longitude}
-          latitude={field.latitude}
-          anchor="bottom"
-          onClick={(e) => {
-            e.originalEvent.stopPropagation();
-            setPopupInfo(field);
-          }}
-        >
-          <Pin />
-        </Marker>
-      )),
-    []
-  );
+  const pins = fields?.map((field) => (
+    <Marker
+      key={`marker-${field.id}`}
+      longitude={field.longitude}
+      latitude={field.latitude}
+      anchor="bottom"
+      onClick={(e) => {
+        e.originalEvent.stopPropagation();
+        setPopupInfo(field);
+      }}
+    >
+      <Pin />
+    </Marker>
+  ));
 
-  const onMoveMapEnd = (longitude, latitude, zoom) => {
-      const radius = zoom/100
-      const fields = useFetchCircle(`http://localhost:8000/fields/location/${longitude}&${latitude}&${radius}`)
-      console.log(fields)
-  }  
+  async function useFetchCircle(
+    url = `http://localhost:8000/fields/location/${longitude}&${latitude}&${radius}`
+  ) {
+    await fetch(url)
+      .then((response) => response.json())
+      .then((response) => {
+        setFields(response);
+      })
+      .catch(() => {
+        console.log("error fetching data");
+      });
+  }
+
+  async function onMoveMapEnd(longitude, latitude, zoom) {
+    const radius = 1 / (zoom);
+    console.log(radius)
+    await useFetchCircle(
+      `http://localhost:8000/fields/location/${longitude}&${latitude}&${radius}`
+    );
+    console.log(fields);
+  }
 
   return (
     <>
       <Map
-        ref={mapRef}
         initialViewState={{
           latitude: lat,
           longitude: lng,
@@ -66,9 +73,15 @@ const MapRender = ({ userLongitude, userLatitude }) => {
           maxBounds: bounds,
         }}
         style={{ width: "100vw", height: "100vh" }}
-        mapStyle="mapbox://styles/mapbox/light-v10"
+        mapStyle="mapbox://styles/mapbox/streets-v11"
         mapboxAccessToken={TOKEN}
-        onMoveEnd={(e) => onMoveMapEnd(e.viewState.longitude, e.viewState.latitude, e.viewState.zoom)}
+        onMoveEnd={(e) =>
+          onMoveMapEnd(
+            e.viewState.longitude,
+            e.viewState.latitude,
+            e.viewState.zoom
+          )
+        }
       >
         <GeolocateControl position="top-left" />
         <FullscreenControl position="top-left" />
