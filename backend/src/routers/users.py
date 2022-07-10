5ffec0eeacf5ad5570dfb6ef861141a99faca3f2
Martin SION <47590798+domino659@users.noreply.github.com>
2022-07-10 15:42:52 +0200

    Firebase beginning

--- backend/src/routers/users.py
+++ backend/src/routers/users.py
@@ -1,3 +1,5 @@
+from firebase_admin import auth
+from fastapi import Depends
 from uuid import UUID
 from fastapi.responses import JSONResponse
 from sqlalchemy.exc import IntegrityError
@@ -5,6 +7,7 @@ from fastapi import APIRouter
 from typing import List
 from fastapi.exceptions import HTTPException
 
+from utils.firebase import CustomSecurity
 from database.db_engine import engine
 from schema.user import User, UserCreate
 from manager import UserManager
@@ -18,6 +21,7 @@ router = APIRouter(
 
 
 # Get all user
+
 @router.get("", response_model=List[User | None])
 def get_all_users():
     with engine.begin() as conn:
@@ -36,6 +40,8 @@ def get_user(user_id: str):
 
 
 # Get id by email
+
+
 @router.get("/get/{email}")
 def get_email(email: str):
     with engine.begin() as conn:
@@ -47,8 +53,11 @@ def get_email(email: str):
 
 
 # Create User
+
+
 @router.post("/create")
-def create_user(user: UserCreate):
+def create_user(user: UserCreate, currentuser=Depends(CustomSecurity)):
+    print(currentuser)
     with engine.begin() as conn:
         try:
             UserManager.create_user(conn, user)
@@ -61,7 +70,7 @@ def create_user(user: UserCreate):
 
 
 # Update user by id
-@router.put("/update/{id}")
+@ router.put("/update/{id}")
 def update_user(user: UserCreate, id: UUID):
     with engine.begin() as conn:
         UserManager.update_user(conn, user, id)
@@ -72,7 +81,7 @@ def update_user(user: UserCreate, id: UUID):
 
 
 # Delete one user by id
-@router.delete("/delete/{user_id}", response_model=bool)
+@ router.delete("/delete/{user_id}", response_model=bool)
 def delete_user(user_id: str):
     with engine.begin() as conn:
         user = UserManager.delete_user_by_id(conn, user_id)
---
+++ backend/src/utils/firebase.py
@@ -0,0 +1,52 @@
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi import Depends, HTTPException, status, Response
+from firebase_admin import auth, credentials
+import firebase_admin
+
+cred = credentials.Certificate('account_key.json')
+firebase_admin.initialize_app(cred)
+
+
+def authentificate(cred):
+    if cred is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Bearer authentication required",
+            headers={'WWW-Authenticate': 'Bearer realm="auth_required"'},
+        )
+    try:
+        decoded_token = auth.verify_id_token(cred.credentials)
+    except Exception as err:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Invalid authentication credentials. {err}",
+            headers={'WWW-Authenticate': 'Bearer error="invalid_token"'},
+        )
+    return decoded_token
+
+
+def CustomSecurity(res: Response, cred: HTTPAuthorizationCredentials = Depends(HTTPBearer(auto_error=False))):
+    # decoded_token = authentificate(cred)
+    res.headers['WWW-Authenticate'] = 'Bearer realm="auth_required"'
+    return res
+    # return decoded_token
+
+
+# def CustomSecurity(authorization: str = Depends(HTTPBearer(auto_error=False))):
+#     token = authorization.split(" ").pop()
+#     if not token:
+#         raise HTTPException(
+#             status_code=401,
+#             detail="Invalid auth credentials",
+#             headers={"WWW-Authenticate": "Bearer"},
+#         )
+
+    # claims = auth.verify_id_token(token)
+
+
+# @router.post("", response_model=model, summary=f"Create {collection}")
+# def post_document(
+#     data: model,
+#     current_user: auth.AuthorizationResponse = Depends(
+#         auth.CustomSecurity),
+# ):
