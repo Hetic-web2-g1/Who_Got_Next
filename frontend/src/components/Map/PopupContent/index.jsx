63f34a4b0f590b526995c8814069056a2aae91eb
Erzenkell <ferrier.sammy@gmail.com>
2022-07-10 14:41:09 +0200

    improvements

--- frontend/src/components/Map/PopupContent/index.jsx
+++ frontend/src/components/Map/PopupContent/index.jsx
@@ -1,32 +1,47 @@
-import React from "react";
-import './styles/style.css'
+import React , {useState} from "react";
+import "./styles/style.css";
 
 const PopupContent = ({ content }) => {
+  const options = [
+    { label: "Acces Handicapé", value: content.handicap ? "oui" : "non" },
+    { label: "Toilettes", value: content.bathroom ? "oui" : "non" },
+    { label: "Douches", value: content.shower ? "oui" : "non" },
+    { label: "Vestiaires", value: content.dressing_room ? "oui" : "non" },
+    { label: "Chauffage", value: content.heating ? "oui" : "non" },
+    { label: "Parking", value: content.parking ? "oui" : "non" },
+    {
+      label: "Transport en commun",
+      value: content.public_transport ? "oui" : "non",
+    },
+  ];
+  const [isOpen, setIsOpen] = useState(false)
+  const onClick = () => setIsOpen(!isOpen);
   return (
     <>
       <div className="popup-container">
-        <div className="heading">
+        <div className="popup-heading">
           <img
-            width="100%"
+            className="image"
+            width="30%"
             src={
               content?.image_path
                 ? content?.image_path
                 : "../../../assets/kirbok.jpg"
             }
           />
-          {content.facility_name}
-          {content.type}
+          <div className="popup-heading-detail">
+            <span className="facility_name">{content.facility_name}</span>
+            <span className="facility_type">{content.type}</span>
+            <div className="dropdown-header" onClick={onClick}>Details</div>
+          </div>
         </div>
-        Acces Handicapé : {content.handicap ? "oui" : "non"}
-        Toilettes : {content.bathroom ? "oui" : "non"}
-        Douches : {content.shower ? "oui" : "non"}
-        Vestiaires : {content.dressing_room ? "oui" : "non"}
-        Chauffage : {content.heating ? "oui" : "non"}
-        Parking : {content.parking ? "oui" : "non"}
-        Transport en commun : {content.public_transport ? "oui" : "non"}
-        <a target="_new" href={`https://www.youtube.com/watch?v=dQw4w9WgXcQ`}>
-          Infos
-        </a>
+        {isOpen && (
+          <div className="popup-content">
+              {options.map((option) => (
+                <div> {option.label} : {option.value}</div>
+              ))}
+          </div>
+        )}
       </div>
     </>
   );
