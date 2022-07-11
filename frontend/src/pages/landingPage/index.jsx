8c9b6018490636746bb27ba99e68d0f7957b2c78
Erzenkell <ferrier.sammy@gmail.com>
2022-07-11 05:56:48 +0200

    PLS

--- frontend/src/pages/landingPage/index.jsx
+++ frontend/src/pages/landingPage/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Banderole from '../../components/banderole';
 import LandingRedirectionButton from '../../components/landingRedirectionButton';
+import "../../fonts/Monument/PPMonumentExtended-Black.otf";
+import "../../fonts/Monument/PPMonumentExtended-Regular.otf";
 import "./styles/style.css";
 
 export const LandingPage = () => {
@@ -14,10 +16,10 @@ export const LandingPage = () => {
                         <h2 className='titletext'>Who Got Next<br/>Cherchez votre partenaire</h2>
                         <p className='lorem'>Lorem ipsum dolor sit amet, consectetur adipiscing Morbin' Martin Time : Rise of Gru 4 Endgame. Duis pulvinar mauris est faucibus. Vestibulum un urna, lacinia eu sit amet, tempus porta ante. Nullam in velit vitae risus aliquet. </p>
                     </div>
+                    <LandingRedirectionButton goto={"login"} innerButton={"Se connecter"}/>
                 </div>
-                <LandingRedirectionButton goto={"login"} innerButton={"Se connecter"}/>
+                <Banderole/>
             </div>
-            <Banderole/>
         </div>
     )
 }
