86a6785c289371f3659b008b351faa14ba0f96a6
Martin SION <47590798+domino659@users.noreply.github.com>
2022-07-11 05:21:20 +0200

    fix route user2

--- backend/src/routers/users.py
+++ backend/src/routers/users.py
@@ -1,8 +1,6 @@
-from uuid import UUID
 from fastapi.security import APIKeyHeader
 from fastapi import APIRouter, HTTPException, Depends
 from typing import List
-
 from firebase_admin import auth
 
 from utils.firebase import SecurityCheck
@@ -25,11 +23,11 @@ def get_all_users():
 
 
 # Get user by id
-@router.get("/{user_id}", response_model=User)
-def get_user(user_id: str, authentified_user=Depends(SecurityCheck)):
+@router.get("/{uid}", response_model=User)
+def get_user(uid: str, authentified_user=Depends(SecurityCheck)):
     with engine.begin() as conn:
-        if user_id == authentified_user.id or authentified_user.is_admin:
-            user = UserManager.get_user_by_id(conn, user_id)
+        if authentified_user.id == uid or authentified_user.is_admin:
+            user = UserManager.get_user_by_id(conn, uid)
             if user is None:
                 raise HTTPException(404, "User not found")
             else:
@@ -52,20 +50,20 @@ def create_user(create_user: UserCreate, bearer_token: str = Depends(APIKeyHeade
 
 
 # Update user by id
-@router.put("/update/{id}")
-def update_user(user: UserCreate, id: str, authentified_user: str = Depends(SecurityCheck)):
+@router.put("/update/{uid}")
+def update_user(user: UserCreate, uid: str, authentified_user: str = Depends(SecurityCheck)):
     with engine.begin() as conn:
-        if authentified_user.id == id:
-            return UserManager.update_user(conn, user, id)
+        if authentified_user.id == uid:
+            return UserManager.update_user(conn, user, uid)
         else:
             raise HTTPException(404, "User not found")
 
 
 # Delete one user by id
-@router.delete("/delete/{user_id}", response_model=bool)
-def delete_user(user_id: str, authentified_user=Depends(SecurityCheck)):
+@router.delete("/delete/{uid}", response_model=bool)
+def delete_user(uid: str, authentified_user=Depends(SecurityCheck)):
     with engine.begin() as conn:
-        if user_id == authentified_user.id or authentified_user.is_admin:
-            return UserManager.delete_user_by_id(conn, user_id)
+        if authentified_user.id == uid or authentified_user.is_admin:
+            return UserManager.delete_user_by_id(conn, uid)
         else:
             raise HTTPException(403, "Action not permitted")
