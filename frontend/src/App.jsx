813abaf9292a63d2774fd1a08f418fe59f310c3a
Erzenkell <ferrier.sammy@gmail.com>
2022-07-11 19:24:27 +0200

    removed useless comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,11 @@
 import Navbar from "./components/sidebar/Navbar";
-import Favsport from "./components/favsport"; 
 import {LandingPage} from "./pages/landingPage";
-import Mysport from "./components/mysports";
-
 
 export default function App() {
   return (
     <main>
       <Navbar/>
       <LandingPage/>
-      {/* <Mysport/>
-      <Favsport/> */}
-      
     </main>
   )
 }
