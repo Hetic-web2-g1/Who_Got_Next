b0e5af4060cddea55b34b41b1a892247ba51e2de
Erzenkell <75608041+Erzenkell@users.noreply.github.com>
2022-07-11 19:29:28 +0200

    stop me mettre ce file de mort

--- frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Homepage from "./pages/homepage"
-
-export default function App() {
-  return (
-    <Homepage />
-
-  )
-}
