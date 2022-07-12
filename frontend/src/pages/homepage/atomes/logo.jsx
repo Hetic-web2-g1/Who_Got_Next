f058a64be8ce7c0d860f3ba7a0c88f9cc3892b9a
Martin SION <47590798+domino659@users.noreply.github.com>
2022-07-12 13:19:59 +0200

    link to home on logo

--- frontend/src/pages/homepage/atomes/logo.jsx
+++ frontend/src/pages/homepage/atomes/logo.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import logo from '../img/logo.svg'
+import { Link } from "react-router-dom";
+
 
 function Logo() {
   return (
-    <div className='logoHome'>
-      <img src={logo} alt="Logo" />
-    </div>
+    <Link to='/home' className="spam">
+      <div className='logoHome'>
+        <img src={logo} alt="Logo" />
+      </div>
+    </Link>
   )
 }
 
