397394ccd49c215394a1951b362b39e5bbd8b454
Spudwebb96 <jonas.janko17@gmail.com>
2022-07-12 15:09:13 +0200

    fix text

--- frontend/src/pages/landingPage/index.jsx
+++ frontend/src/pages/landingPage/index.jsx
@@ -14,9 +14,9 @@ export const LandingPage = () => {
                     <div className='container'>
                         <h4 className='asktext'>Ask</h4>
                         <h2 className='titletext'>Who Got Next<br/>Cherchez votre partenaire</h2>
-                        <p className='lorem'>Nouveau en ville et tu ne sais pas où jouer ? Ne te demande plus. Demande Who Got Next ?
-Notre carte interactive est remplie avec des spots vérifiés par nos utilisateurs et notre communauté pour trouver où et avec qui pratiquer votre sport favori.
-Basketball, Course, Football, Handball, Volleyball, Handisport... Rejoignez notre communauté, trouvez votre équipe et partagez vos terrains préférés ! </p>
+                        <p className='lorem'>Nouveau en ville et tu ne sais pas où jouer ? Ne te demande plus. Demande Who Got Next ? <br></br>
+Notre carte interactive est remplie avec des spots vérifiés par nos utilisateurs et notre communauté pour trouver où et avec qui pratiquer votre sport favori.<br></br>
+Basketball, Course, Football, Handball, Volleyball, Handisport...<br></br> Rejoignez notre communauté, trouvez votre équipe et partagez vos terrains préférés ! </p>
                     </div>
                     <LandingRedirectionButton goto={"login"} innerButton={"Se connecter"}/>
                 </div>
