2a7feda009fd08023a12abf2dbd100c90b9a6062
Martin SION <47590798+domino659@users.noreply.github.com>
2022-07-12 13:27:38 +0200

    fix page join and delete useless info

--- frontend/src/pages/joinpage/components/header_join.jsx
+++ frontend/src/pages/joinpage/components/header_join.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import Logo from '../../homepage/atomes/logo';
-import RouterJoin from './router_join';
-import Account from '../../homepage/atomes/account'
 import PlayJoin from './play_join'
 import Sports from '../../homepage/atomes/sport';
 
@@ -9,11 +6,6 @@ export const HeaderJoin = () => {
     return (
         <>
             <div className='header' >
-                <div className='logoAccount' >
-                    <Logo />
-                    <RouterJoin />
-                    <Account />
-                </div>
 
                 <div className='play'>
                     <PlayJoin />
